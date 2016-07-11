318bba8bab6642d10de03a8186b0d29ca0218c30
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-07-11 11:56:35 +0200

    first implementation of ram formatting

--- page/ram.go
+++ page/ram.go
@@ -1,14 +1,39 @@
 // TODO add doc
 package page
 
-import "github.com/shirou/gopsutil/mem"
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/mem"
+)
 
 type Ram struct {
-	Total       uint64
-	Available   uint64
-	Used        uint64
-	UsedPercent float64
-	Free        uint64
+	Total       string
+	Available   string
+	Used        string
+	UsedPercent string
+	Free        string
+}
+
+const (
+	kilobyte uint64 = 1024
+	megabyte uint64 = 1048576
+	gigabyte uint64 = 1073741824
+)
+
+// i must be represented in bytes
+func format(i uint64) string {
+	/*if i >= gigabyte {
+		return fmt.Sprintf("%d Gb", i/gigabyte)
+	} else
+	*/
+	if i >= megabyte {
+		return fmt.Sprintf("%d Mb", i/megabyte)
+	} else if i >= kilobyte {
+		return fmt.Sprintf("%d Kb", i/kilobyte)
+	} else {
+		return "error"
+	}
 }
 
 func NewRAM() *Ram {
@@ -16,10 +41,10 @@ func NewRAM() *Ram {
 	// TODO check err
 
 	return &Ram{
-		Total:       vmstat.Total,
-		Available:   vmstat.Available,
-		Used:        vmstat.Used,
-		UsedPercent: vmstat.UsedPercent,
-		Free:        vmstat.Free,
+		Total:       format(vmstat.Total),
+		Available:   format(vmstat.Available),
+		Used:        format(vmstat.Used),
+		UsedPercent: fmt.Sprintf("%.2f", vmstat.UsedPercent),
+		Free:        format(vmstat.Free),
 	}
 }
