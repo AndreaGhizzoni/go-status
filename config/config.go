3f331c504107a4d86de97c2285c343923e1f8b27
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-07-14 17:25:39 +0200

    now empty configuration file is created

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 
 	"github.com/AndreaGhizzoni/go-status/constant"
+	"github.com/AndreaGhizzoni/go-status/util"
 )
 
 // TODO add doc
@@ -12,19 +13,31 @@ type Config struct {
 	Watchedpaths []string `json:"wp"`
 }
 
-var instance *Config
+var (
+	defCfg string = "{ \"wp\" : [] }"
+)
 
 // TODO add doc
 func Parse() (*Config, error) {
-	file, err := ioutil.ReadFile(constant.ConfigPath)
-	if err != nil {
-		return nil, err
+	c := &Config{}
+
+	if e, _ := util.ExistsPath(constant.ConfigPath); !e {
+		err := ioutil.WriteFile(constant.ConfigPath,
+			[]byte(defCfg), 0644)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		file, err := ioutil.ReadFile(constant.ConfigPath)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal(file, c)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	c := &Config{}
-	err = json.Unmarshal(file, c)
-	if err != nil {
-		return nil, err
-	}
 	return c, nil
 }
